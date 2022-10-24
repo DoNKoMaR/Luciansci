315ea87b6193311b3bf82882bf9a8a33fa5dcad4
KoMaR005 <113529621+DoNKoMaR@users.noreply.github.com>
2022-10-24 19:03:41 +0600

    Lucian

---
+++ plugins/random-cat.js
@@ -0,0 +1,18 @@
+import fetch from 'node-fetch'
+let handler  = async (m, { conn, text }) => {
+try {
+let res = await fetch('https://cataas.com/cat')
+let img = await res.buffer()
+let caption = `
+_★Люциан - 𝙱𝚘𝚝★- Bot_
+`.trim()
+conn.sendFile(m.chat, img, 'cat.jpg', caption, m)
+} catch (e) {
+console.log(e)
+throw '*Error!*'
+}}
+handler.help = ['cat']
+handler.tags = ['random']
+handler.command = /^котейка$/i
+handler.fail = null
+export default handler
